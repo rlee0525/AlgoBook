2b3ad5e846dcefdb6cd1ec50a2c7275ce787704b
Raymond Lee <raylee0525@gmail.com>
2017-08-16 15:32:59 -0700

    boards

---
+++ app/controllers/api/boards_controller.rb
@@ -0,0 +1,23 @@
+class Api::BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      render 'api/boards/show'
+    else
+      render json: @board.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def board_params
+    params
+      .require(:board)
+      .permit(:category, :description, :created_at)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resources :boards
+  end
+
   root "static_pages#root"
 end
