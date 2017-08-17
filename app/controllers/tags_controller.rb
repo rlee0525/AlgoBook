98c55c0a637fb07b79de9c2ad608f683d3b76f86
Raymond Lee <raylee0525@gmail.com>
2017-08-17 09:55:59 -0700

    tag controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,43 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      render 'api/tags/show'
+    else
+      render json: @tag.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    render json: @tag
+  end
+
+  def search
+    if params[:query]
+      @tags = Tag.all.where("LOWER(name) ~ LOWER(?)", params[:query])
+    else
+      @tags = Tag.none
+    end
+
+    render :search
+  end
+
+  private
+
+  def tag_params
+    .require(:tag)
+    .permit(:name, :post_id)
+  end
 end
